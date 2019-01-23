99991f92b17fbec04c0d52487374c3a7dedd18d0
Douglas Swiatocha <dswiatocha@gmail.com>
2019-01-23 11:49:52 -0600

    Added functionality to return encrypter string in text area

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form="""
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,7 +31,7 @@ form="""
                 Rotate by:
                 <input type="text" name="rot" value="0"/>
             </label>
-            <textarea placeholder="Type your sentence here" name="text"></textarea>
+            <textarea placeholder="Type your sentence here!" name="text">{0}</textarea>
             <input type="submit" value="Encode"/>
     </body>
 </html>
@@ -39,13 +39,13 @@ form="""
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/", methods=["POST"])
 def encrypt():
     rot = int(request.form["rot"])
     text = request.form["text"]
     newtext = rotate_string(text, rot)
-    return "<h1>" + newtext + "</h1>"
+    return form.format(newtext)
 
 app.run()
